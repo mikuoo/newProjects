96ee8527e9c969402f973a51c1c19ad95a3f13f2
mikucc <2149771396@qq.com>
2021-11-30 21:20:48 +0800

    otherDemo commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+
+	public static void main(String[] args) {
+		System.out.println("2");
+		System.out.println("hellow");
+		System.out.println("1");
+		System.out.println("hello");
+	}
+	
+	
+}
